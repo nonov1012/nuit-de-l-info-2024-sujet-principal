fdcfc6be786c4e0bae4ead022f8587ec66db8fc5
Keylian Turbé <keylian.turbe@univ-littoral.fr>
2024-12-06 01:22:34 +0100

    Changement badui

--- nuit_info_2024/badui.php
+++ nuit_info_2024/badui.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AI BUD</title> 
+    <title>AI BUD</title>
     <link rel="stylesheet" href="css/badui.css">
 </head>
 <?php
@@ -14,7 +14,7 @@ include dirname(__FILE__) . "/inc/header.php";
     <script>
         document.addEventListener('keydown', (event) => {
             const message = document.getElementById('message');
-            
+
             if (event.key === 'ArrowLeft') {
                 message.textContent = 'Flèche gauche appuyée';
             } else if (event.key === 'ArrowRight') {
