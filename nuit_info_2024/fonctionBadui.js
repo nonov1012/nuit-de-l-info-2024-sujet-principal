b03ea06505dfd0b7eec19febfaf6ca2916029c63
Keylian Turbé <keylian.turbe@univ-littoral.fr>
2024-12-06 06:06:50 +0100

    Feature fini sur le theme

--- nuit_info_2024/fonctionBadui.js
+++ nuit_info_2024/fonctionBadui.js
@@ -1,4 +1,4 @@
-function styleToRadio(body) {
+function styleToRadio(body, navbar, footer) {
   body.classList.toggle("radio");
   navbar.classList.toggle("radio");
   footer.classList.toggle("radio");
@@ -9,40 +9,47 @@ function styleToInfrarouge() {
   footer.classList.toggle("infrarouge");
   body.classList.toggle("infrarouge");
 }
+
 function styleToNocturne() {
   body.classList.toggle("nocturne");
   navbar.classList.toggle("nocturne");
   footer.classList.toggle("nocturne");
 }
-const compteur = 0;
+
+function styleToBasique() {
+  body.classList.toggle("basique");
+  navbar.classList.toggle("basique");
+  footer.classList.toggle("basique");
+}
+
+let compteur = 0;
+const message = document.getElementById("message");
 
 document.addEventListener("keydown", (event) => {
-  if (event.ctrlKey && event.shiftKey && event.key === "T") {
+  const body = document.body;
+  const navbar = document.querySelector(".navbar");
+  const footer = document.querySelector(".footer");
+
+  if (event.altKey && event.key === "h") {
     compteur += 1;
-  }
-  switch (compteur) {
-    case 1:
-      const body = document.getElementById("body");
-
-      styleToRadio(body, navbar);
-      break;
-
-      case 2 :
-        
-
-    default:
-      break;
-  }
-  if (event.key === "ArrowLeft") {
-    message.textContent = "Flèche gauche appuyée";
-  } else if (event.key === "ArrowRight") {
-    styleToInfrarouge();
-    message.textContent = "Flèche droite appuyée";
-  } else if (event.key === "ArrowDown") {
-    styleToNocturne();
-    message.textContent = "Flèche bas appuyée";
-  } else {
-    styleToBasique();
-    message.textContent = "Autre touche appuyée";
+    if (compteur > 4) {
+      compteur = 1;
+    }
+    switch (compteur) {
+      case 1:
+        styleToRadio(body, navbar, footer);
+        break;
+      case 2:
+        styleToRadio(body, navbar, footer);
+        styleToInfrarouge();
+        break;
+      case 3:
+        styleToInfrarouge();
+        styleToNocturne();
+        break;
+      case 4:
+        styleToNocturne();
+        break;
+    }
   }
 });
